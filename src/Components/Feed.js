4e6c98198ee530370a90248665bdf6e371509555
jason002009 <jason002009@berkeley.edu>
2022-11-04 18:02:34 -0700

    f

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -4,17 +4,18 @@ import Menu from './Menu';
 import Block from './Block';
 
 const Feed = () => {
-    /* TODO: Declare a new state variable to keep track of the blocks on your Blockstagram feed! */
-    // Refer to Hint 2 for more help!
 
-    /* Use the map() function to render multiple Blocks! */
-    const posts = null; // TODO: edit this variable
+    const [blocks, setBlocks] = useState([]);
 
+    const update = (boxColor) => {
+        setBlocks(posts => [boxColor, ...posts]);
+    };
+
+    const posts = blocks.map((col) => <Block color={col} caption={col}></Block>);
+        
     return (
         <div>
-            <Menu></Menu>
-
-            {/* Below is where all of your Blocks should render! */}
+            <Menu onClick={update}></Menu>
             {posts}
         </div>
     );
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from "react";
+import Color from "./Color";
+import Feed from "./Feed";
 /* Add any imports you think you might need here! */
 
-const Menu = () => { 
+const Menu = () => {
+  const [state, Feed] = useState([]);
 
-    return (
-      <div className="colorOptions">
-          {/* TODO */}
-      </div>
-    );
-}
+  return (
+    <div className="colorOptions">
+      <Color
+        color="red"
+        handleClick={(color) => Feed({ state: [color, ...state] })}
+      ></Color>
+      <Color
+        color="pink"
+        handleClick={(color) => Feed({ state: [color, ...state] })}
+      ></Color>
+      <Color
+        color="blue"
+        handleClick={(color) => Feed({ state: [color, ...state] })}
+      ></Color>
+      <Color
+        color="green"
+        handleClick={(color) => Feed({ state: [color, ...state] })}
+      ></Color>
+    </div>
+  );
+};
 
 export default Menu;
